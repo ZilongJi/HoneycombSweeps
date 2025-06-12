dd9245c160a3403fe45ff2f412ee91a9f7fdc88f
ZilongJi <jzlxmu@163.com>
2025-06-12 23:14:04 +0100

    update model

--- models.py
+++ models.py
@@ -505,3 +505,318 @@ class PC_cell_topdown_asym(bp.DynamicalSystem):
         #get the center of the bump from self.r which is num x num matrix
         self.center[1], self.center[0] = bm.unravel_index(bm.argmax(self.r.value), [self.num, self.num])
         
+class HD_cell_L1(bp.DynamicalSystem):
+    def __init__(
+        self,
+        num,
+        noise_stre=0.01,
+        tau=1.0,
+        tau_v=10.0,
+        k=1.0,
+        mbar=15.,
+        a=0.4,
+        A=3.0,
+        J0=4.0,
+        z_min=-bm.pi,
+        z_max=bm.pi,
+        goal_a=0.5,
+        goal_A=1.0,
+        goal_dir=0,
+        topdown=False,
+    ):
+        super(HD_cell_L1, self).__init__()
+
+        # parameters
+        self.tau = tau  # The synaptic time constant
+        self.tau_v = tau_v
+        self.k = k / num * 20  # Degree of the rescaled inhibition
+        self.a = a  # Half-width of the range of excitatory connections
+        self.A = A  # Magnitude of the external input
+        self.J0 = J0 / num * 20  # maximum connection value
+        self.m = mbar * tau / tau_v
+        self.noise_stre = noise_stre
+
+        # neuron num
+        self.num = num  # head-direction cell
+        # feature space
+        self.z_min = z_min
+        self.z_max = z_max
+        self.z_range = z_max - z_min
+        x1 = bm.linspace(z_min, z_max, num + 1)  # The encoded feature values
+        self.x = x1[0:-1]
+        
+        self.topdown = topdown #if turn on the topdown input
+        self.goal_dir = goal_dir #direction to the goal
+        self.goal_a = goal_a #the half-width of the range of top down connections from the reward cell
+        self.goal_A = goal_A #the magnitude of the top down input       
+
+        # connection matrix
+        conn_mat = self.make_conn()
+        self.conn_fft = bm.fft.fft(conn_mat)
+
+        # neuron state variables
+        self.r = bm.Variable(bm.zeros(num))
+        self.u = bm.Variable(bm.zeros(num))  # head direction cell
+        self.v = bm.Variable(bm.zeros(num))
+        self.input = bm.Variable(bm.zeros(num))
+        self.center = bm.Variable(bm.zeros(1))
+        self.centerI = bm.Variable(bm.zeros(1))
+        self.total_input = bm.Variable(bm.zeros(num))
+
+        # 定义积分器
+        self.integral = bp.odeint(method="exp_euler", f=self.derivative)
+
+    def topdown_input(self, Topdown_mod):
+        # return bump input which is a one-dim bump (same dim as neuroal space)
+        assert bm.size(self.goal_dir) == 1
+
+        d = self.dist(bm.asarray(self.goal_dir) - self.x)
+        # d = bm.linalg.norm(d)
+        # Gaussian bump input
+        input_ = self.goal_A * bm.exp(-0.25 * bm.square(d / self.goal_a))
+
+        # further topdown modulation
+        input_ = input_ * Topdown_mod
+
+        return input_
+
+    def dist(self, d):
+        d = self.circle_period(d)
+        d = bm.where(d > 0.5 * self.z_range, d - self.z_range, d)
+        return d
+
+    def make_conn(self):
+        d = self.dist(bm.abs(self.x[0] - self.x))
+        Jxx = self.J0 * bm.exp(-0.5 * bm.square(d / self.a)) / (2 * bm.pi * self.a ** 2)
+        return Jxx
+
+    def get_center(self, r, x):
+        exppos = bm.exp(1j * x)
+        center = bm.angle(bm.sum(exppos * r))
+        return center.reshape(-1,)
+
+    @property
+    def derivative(self):
+        du = lambda u, t, input: (-u + input - self.v) / self.tau
+        dv = lambda v, t: (-v + self.m * self.u) / self.tau_v
+        return bp.JointEq([du, dv])
+
+    def circle_period(self, A):
+        B = bm.where(A > bm.pi, A - 2 * bm.pi, A)
+        B = bm.where(B < -bm.pi, B + 2 * bm.pi, B)
+        return B
+
+    def input_HD(self, HD):
+        # integrate self motion
+        return self.A * bm.exp(-0.25 * bm.square(self.dist(self.x - HD) / self.a))
+
+    def reset_state(self, HD_truth):
+        self.r.value = bm.Variable(bm.zeros(self.num))
+        self.u.value = bm.Variable(bm.zeros(self.num))
+        self.v.value = bm.Variable(bm.zeros(self.num))
+        self.center.value = bm.Variable(bm.zeros(1,) + HD_truth)
+
+    def update(self, HD, ThetaInput, Topdown_mod):
+        
+        
+        self.center = self.get_center(r=self.r, x=self.x)
+        Iext = ThetaInput * self.input_HD(HD)
+        
+        if self.topdown: #top down control
+            self.td_input = self.topdown_input(Topdown_mod)
+            self.total_input = Iext + self.td_input
+        else:
+            self.total_input = Iext
+        
+        # Calculate input
+        r_fft = bm.fft.fft(self.r)
+        Irec = bm.real(bm.fft.ifft(r_fft * self.conn_fft))
+        input_total = self.total_input + Irec + bm.random.randn(self.num) * self.noise_stre
+        
+        # Update neural state
+        u, v = self.integral(self.u, self.v, bp.share.load("t"), input_total, bm.dt)
+        
+        self.u.value = bm.where(u > 0, u, 0)
+        self.v.value = v
+        r1 = bm.square(self.u)
+        r2 = 1.0 + self.k * bm.sum(r1)
+        self.r.value = r1 / r2
+        
+class PC_cell_L2(bp.DynamicalSystem):
+    def __init__(
+        self,
+        noise_stre=0.,
+        num=100,
+        tau=10.0,
+        tau_v=100.0,
+        mbar=75.0,
+        a=0.5,
+        A=1.0,
+        J0=5.0,
+        k=1,
+        g = 1000,
+        x_min=-bm.pi,
+        x_max=bm.pi,
+        num_hd = 100,
+        Phase_Offset = 1/9,
+    ):
+        super(PC_cell_L2, self).__init__()
+
+        # dynamics parameters
+        self.tau = tau  # The synaptic time constant
+        self.tau_v = tau_v  # The time constant of the adaptation variable
+        self.num_x = num  # number of excitatory neurons for x dimension
+        self.num_y = num  # number of excitatory neurons for y dimension
+        self.num = self.num_x * self.num_y
+        self.num_hd = num_hd
+        self.k = k  # Degree of the rescaled inhibition
+        self.a = a  # Half-width of the range of excitatory connections
+        self.A = A  # Magnitude of the external input
+        self.g = g
+        self.J0 = J0/g  # maximum connection value
+        self.m = mbar * tau / tau_v
+        self.noise_stre = noise_stre
+        self.Phase_Offset = Phase_Offset
+
+        # feature space
+        self.x_range = x_max - x_min
+        phi_x = bm.linspace(x_min, x_max, self.num_x + 1)  # The encoded feature values
+        self.x = phi_x[0:-1]
+        self.y_range = self.x_range
+        phi_y = bm.linspace(x_min, x_max, self.num_y + 1)  # The encoded feature values
+        self.y = phi_y[0:-1]
+        x_grid, y_grid = bm.meshgrid(self.x, self.y)
+        self.x_grid = x_grid.flatten()
+        self.y_grid = y_grid.flatten()
+        self.value_grid = bm.stack([self.x_grid, self.y_grid]).T
+
+        # initialize conn matrix
+        self.conn_mat = self.make_conn()
+        
+        # initialize dynamical variables
+        self.r = bm.Variable(bm.zeros(self.num))
+        self.u = bm.Variable(bm.zeros(self.num))
+        self.v = bm.Variable(bm.zeros(self.num))
+        self.input = bm.Variable(bm.zeros(self.num))
+        self.center_I = bm.Variable(bm.zeros(2))
+        self.center_bump = bm.Variable(bm.zeros(2))
+
+        # 定义积分器
+        self.integral = bp.odeint(method="exp_euler", f=self.derivative)
+
+    def circle_period(self, d):
+        d = bm.where(d > bm.pi, d - 2 * bm.pi, d)
+        d = bm.where(d < -bm.pi, d + 2 * bm.pi, d)
+        return d
+
+    def dist(self, d):
+        dis = self.circle_period(d)
+        delta_x = dis[:, 0]
+        delta_y = dis[:, 1]
+        dis = bm.sqrt(delta_x ** 2 + delta_y ** 2)
+        return dis
+
+    def make_conn(self):
+        @jax.vmap
+        def get_J(v):
+            d = self.dist(v - self.value_grid)
+            Jxx = (
+                self.J0
+                * bm.exp(-0.5 * bm.square(d / self.a))
+                / (bm.sqrt(2 * bm.pi) * self.a)
+            )
+            return Jxx
+
+        return get_J(self.value_grid)
+
+    def Postophase(self, pos):
+        phase = pos + bm.pi  # 坐标变换
+        phase_x = bm.mod(phase[0], 2 * bm.pi) - bm.pi
+        phase_y = bm.mod(phase[1], 2 * bm.pi) - bm.pi
+        Phase = bm.array([phase_x, phase_y])
+        return Phase
+    
+
+    def input_by_conjG(self, Animal_location, HD_activity, ThetaModulator, HD_truth):
+        assert bm.size(Animal_location) == 2
+        num_hd = self.num_hd
+        hd = bm.linspace(-bm.pi,bm.pi,num_hd) 
+        # each head-direction cell corresponds to a group of Conjunctive grid cells, which in turn projects to pure grid cells with assymetric connections determined by offset(hd)
+        lagvec = -bm.array([bm.cos(HD_truth), bm.sin(HD_truth)]) * self.Phase_Offset * 1.4
+        offset = bm.array([bm.cos(hd), bm.sin(hd)]) * self.Phase_Offset + 0*lagvec.reshape(-1,1)
+        self.center_conjG = self.Postophase(
+            Animal_location.reshape([-1,1]) + offset.reshape(-1,num_hd)
+        )  # Ideal phase using mapping function
+        input = bm.zeros([num_hd, self.num])
+        for i in range(num_hd):
+            d = self.dist(bm.asarray(self.center_conjG[:,i]) - self.value_grid)
+            input[i] = self.A * bm.exp(-0.25 * bm.square(d / self.a))
+            
+        max_hd = bm.max(HD_activity)
+        hd_weight = bm.where(HD_activity>max_hd/3, HD_activity, 0)
+        hd_weight = hd_weight/bm.sum(hd_weight)
+
+        total_input = bm.matmul(input.transpose(), hd_weight).reshape(-1,) * ThetaModulator
+        
+        #add animal location as a sensory inout which do not receive theta modulation
+        loc_ = self.Postophase(Animal_location)
+        d = self.dist(bm.asarray(loc_) - self.value_grid)
+        loc_input = self.A * bm.exp(-0.25 * bm.square(d / self.a))
+        total_input = total_input + loc_input
+        
+        return total_input
+
+
+    def get_center(self):
+        exppos_x = bm.exp(1j * self.x_grid)
+        exppos_y = bm.exp(1j * self.y_grid)
+        r = bm.where(self.r > bm.max(self.r) * 0.1, self.r, 0)
+        
+        #get the center of the activity bump
+        self.center_bump[0] = bm.angle(bm.sum(exppos_x * r))
+        self.center_bump[1] = bm.angle(bm.sum(exppos_y * r))
+        
+        #get the center of the moddle layer input (offset input)
+        self.center_I[0] = bm.angle(bm.sum(exppos_x * self.input))
+        self.center_I[1] = bm.angle(bm.sum(exppos_y * self.input))
+
+
+    @property
+    def derivative(self):
+        du = (
+            lambda u, t, Irec: (
+                -u
+                + Irec
+                + self.input
+                - self.v
+            )
+            / self.tau
+        )
+        dv = lambda v, t: (-v + self.m * self.u) / self.tau_v
+        return bp.JointEq([du, dv])
+
+    def reset_state(self):
+        self.r.value = bm.Variable(bm.zeros(self.num))
+        self.u.value = bm.Variable(bm.zeros(self.num))
+        self.v.value = bm.Variable(bm.zeros(self.num))
+        self.input.value = bm.Variable(bm.zeros(self.num))
+
+    def update(self, Animal_location, HD_activity, ThetaModulator, HD_truth):
+        #update the center and store them
+        self.get_center()
+        
+        #
+        input_conjG = self.input_by_conjG(Animal_location, HD_activity, ThetaModulator, HD_truth)
+        
+        self.input = input_conjG
+        
+        Irec = bm.matmul(self.conn_mat, self.r) + self.noise_stre * bm.random.randn(
+            (self.num)
+        )
+        # Update neural state
+        u, v = self.integral(self.u, self.v, bp.share["t"], Irec, bm.dt)
+        self.u.value = bm.where(u > 0, u, 0)
+        self.v.value = v
+        r1 = bm.square(self.u)
+        r2 = 1.0 + self.k * bm.sum(r1)
+        self.r.value = self.g*r1 / r2
